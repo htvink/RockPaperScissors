0203f1d17754e6a0dbe42a8d95487b7d8848eb5a
mortYyY <htvink45@gmail.com>
2026-07-16 00:02:47 +0300

    Add replay option

--- main.cpp
+++ main.cpp
@@ -10,47 +10,59 @@ int main()
 
     srand(time(0));
 
-    int playerChoice;
-    int computerChoice;
+    char again;
 
-    computerChoice = rand() % 3 + 1;
+    do
+    {
+        int playerChoice;
+        int computerChoice;
 
-    std::cout << "=== Камінь, ножиці, папір ===\n";
-    std::cout << "1 - Камінь\n";
-    std::cout << "2 - Ножиці\n";
-    std::cout << "3 - Папір\n";
+        computerChoice = rand() % 3 + 1;
 
-    std::cout << "Твій вибір: ";
-    std::cin >> playerChoice;
+        std::cout << "\n=== Камінь, ножиці, папір ===\n";
+        std::cout << "1 - Камінь\n";
+        std::cout << "2 - Ножиці\n";
+        std::cout << "3 - Папір\n";
 
-    if (computerChoice == 1)
-{
-    std::cout << "Комп'ютер вибрав: Камінь\n";
-}
-else if (computerChoice == 2)
-{
-    std::cout << "Комп'ютер вибрав: Ножиці\n";
-}
-else
-{
-    std::cout << "Комп'ютер вибрав: Папір\n";
-}
-if (playerChoice == computerChoice)
-{
-    std::cout << "Нічия!\n";
-}
-else if (
-    (playerChoice == 1 && computerChoice == 2) ||
-    (playerChoice == 2 && computerChoice == 3) ||
-    (playerChoice == 3 && computerChoice == 1)
-)
-{
-    std::cout << "Ти переміг!\n";
-}
-else
-{
-    std::cout << "Переміг комп'ютер!\n";
-}
+        std::cout << "Твій вибір: ";
+        std::cin >> playerChoice;
+
+        if (computerChoice == 1)
+        {
+            std::cout << "Комп'ютер вибрав: Камінь\n";
+        }
+        else if (computerChoice == 2)
+        {
+            std::cout << "Комп'ютер вибрав: Ножиці\n";
+        }
+        else
+        {
+            std::cout << "Комп'ютер вибрав: Папір\n";
+        }
+
+        if (playerChoice == computerChoice)
+        {
+            std::cout << "Нічия!\n";
+        }
+        else if (
+            (playerChoice == 1 && computerChoice == 2) ||
+            (playerChoice == 2 && computerChoice == 3) ||
+            (playerChoice == 3 && computerChoice == 1)
+        )
+        {
+            std::cout << "Ти переміг!\n";
+        }
+        else
+        {
+            std::cout << "Переміг комп'ютер!\n";
+        }
+
+        std::cout << "\nЗіграти ще раз? (y/n): ";
+        std::cin >> again;
+
+    } while (again == 'y');
+
+    std::cout << "\nДякую за гру!\n";
 
     return 0;
 }
