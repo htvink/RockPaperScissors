4ff72d28ce15a2d1992fb471c5df623a4c555061
mortYyY <htvink45@gmail.com>
2026-07-15 23:52:18 +0300

    Add computer choice and winner logic

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include <windows.h>
 
 int main()
@@ -6,7 +8,12 @@ int main()
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
+    srand(time(0));
+
     int playerChoice;
+    int computerChoice;
+
+    computerChoice = rand() % 3 + 1;
 
     std::cout << "=== Камінь, ножиці, папір ===\n";
     std::cout << "1 - Камінь\n";
@@ -16,7 +23,34 @@ int main()
     std::cout << "Твій вибір: ";
     std::cin >> playerChoice;
 
-    std::cout << "Ти зробив вибір: " << playerChoice << "\n";
+    if (computerChoice == 1)
+{
+    std::cout << "Комп'ютер вибрав: Камінь\n";
+}
+else if (computerChoice == 2)
+{
+    std::cout << "Комп'ютер вибрав: Ножиці\n";
+}
+else
+{
+    std::cout << "Комп'ютер вибрав: Папір\n";
+}
+if (playerChoice == computerChoice)
+{
+    std::cout << "Нічия!\n";
+}
+else if (
+    (playerChoice == 1 && computerChoice == 2) ||
+    (playerChoice == 2 && computerChoice == 3) ||
+    (playerChoice == 3 && computerChoice == 1)
+)
+{
+    std::cout << "Ти переміг!\n";
+}
+else
+{
+    std::cout << "Переміг комп'ютер!\n";
+}
 
     return 0;
 }
